766247b385b4e2868b67f207ccad6424e1405088
ilyasbabu <ilyasbabuvp@gmail.com>
2026-04-17 19:14:27 +0530

    signup, user detail api

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from .views import SignupView, UserDetailView
+
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/signup/', SignupView.as_view(), name='signup'),
+    path('api/user-details/', UserDetailView.as_view(), name='user_detail'),
 ]
