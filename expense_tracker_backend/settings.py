8c0b78c68e510c37b5a50f80b32e3f64a9168447
ilyasbabu <ilyasbabuvp@gmail.com>
2026-04-20 20:21:53 +0530

    allowed hosts updated

--- expense_tracker_backend/settings.py
+++ expense_tracker_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mtm4iiq14aw0x7h74g2*oc%vz+6@*_axu=wlbeu85f!6-a)rd-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["hashirmon22.pythonanywhere.com"]
 
 
 # Application definition
